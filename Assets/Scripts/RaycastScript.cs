06d71e1cac7034c1794a9edd9c077d62147a4dd5
tiberiuioan35@gmail.com <tiberiuioan35@gmail.com>
2023-12-17 15:58:45 +0200

    Add rotation to game object

--- Assets/Scripts/RaycastScript.cs
+++ Assets/Scripts/RaycastScript.cs
@@ -10,6 +10,11 @@ public class RaycastScript : MonoBehaviour
     ARRaycastManager raycastManager;
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
+    private bool isPinching = false;
+    private Vector2 initialTouch0Position;
+    private Vector2 initialTouch1Position;
+    private float initialRotationY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +24,39 @@ public class RaycastScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (Input.touchCount == 2)
+        {
+            Touch touch0 = Input.GetTouch(0);
+            Touch touch1 = Input.GetTouch(1);
+
+            if (touch0.phase == TouchPhase.Began || touch1.phase == TouchPhase.Began)
+            {
+                initialTouch0Position = touch0.position;
+                initialTouch1Position = touch1.position;
+
+                initialRotationY = spawnedObject.transform.eulerAngles.y;
+                isPinching = true;
+            }
+            else if (touch0.phase == TouchPhase.Ended || touch1.phase == TouchPhase.Ended)
+            {
+                isPinching = false;
+            }
+
+            if (isPinching && raycastManager.Raycast(touch0.position, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Vector2 currentTouch0Position = touch0.position;
+                Vector2 currentTouch1Position = touch1.position;
+
+                float initialAngle = Mathf.Atan2(initialTouch1Position.y - initialTouch0Position.y, initialTouch1Position.x - initialTouch0Position.x) * Mathf.Rad2Deg;
+                float currentAngle = Mathf.Atan2(currentTouch1Position.y - currentTouch0Position.y, currentTouch1Position.x - currentTouch0Position.x) * Mathf.Rad2Deg;
+
+                float rotationAngle = currentAngle - initialAngle;
+
+                float newRotationY = initialRotationY - rotationAngle;
+                spawnedObject.transform.rotation = Quaternion.Euler(0f, newRotationY, 0f);
+            }
+        }
+        else if (Input.touchCount > 0)
         {
             if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
             {
@@ -33,10 +70,10 @@ public class RaycastScript : MonoBehaviour
 
     private bool IsPointerOverUIObject()
     {
-        PointerEventData enentDataCurrentPosition = new PointerEventData(EventSystem.current);
-        enentDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         List<RaycastResult> results = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(enentDataCurrentPosition, results);
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
         return results.Count > 0;
     }
 }
